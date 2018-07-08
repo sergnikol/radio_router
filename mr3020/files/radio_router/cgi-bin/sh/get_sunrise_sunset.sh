055403305f303954f57b2da6095ab64fff302686
Your Name <you@example.com>
2018-07-08 23:17:17 +0300

    alarm

--- mr3020/files/radio_router/cgi-bin/sh/get_sunrise_sunset.sh
+++ mr3020/files/radio_router/cgi-bin/sh/get_sunrise_sunset.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-SUNRISE=`wget -q -O - "http://if.limit.com.ua/sun.php?rise"`
-SUNSET=`wget -q -O - "http://if.limit.com.ua/sun.php?set"`
+GPS="&la=48.45&lo=34.98333" #Dnipro
+SUNRISE=`wget -q -O - "http://if.limit.com.ua/sun.php?rise${GPS}"`
+SUNSET=`wget -q -O - "http://if.limit.com.ua/sun.php?set${GPS}"`
 LEN=`echo -n $SUNRISE$SUNSET | wc -L`
 if [  "$LEN" != "10" ]; then
    echo "error time from server"
@@ -13,5 +14,5 @@ sed -i '/alarm_play/d' $CRONFILE
 #SUNSET=`echo $SUNSET | sed 's/^0//g' | sed 's/ 0/ /g'`
 #SUNRISE=`echo $SUNRISE | sed 's/^0//g' | sed 's/ 0/ /g'`
 echo "$SUNSET * * * /radio_router/cgi-bin/sh/alarm_stop.sh > /dev/null 2>&1 #`date`" >> $CRONFILE
-#echo "$SUNRISE * * * /radio_router/cgi-bin/sh/alarm_play.sh > /dev/null 2>&1 #`date`" >> $CRONFILE
+echo "#$SUNRISE * * * /radio_router/cgi-bin/sh/alarm_play.sh > /dev/null 2>&1 #`date`" >> $CRONFILE
 /etc/init.d/cron restart
